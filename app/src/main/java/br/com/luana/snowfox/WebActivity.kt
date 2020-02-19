cd39be00d09e06730948b75ea1305a743ccf355c
luana-vargas <vargas.luana@outlook.com.br>
2020-02-19 19:14:52 -0300

    Código inicial da webview

---
+++ app/src/main/java/br/com/luana/snowfox/WebActivity.kt
@@ -0,0 +1,21 @@
+package br.com.luana.snowfox
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import kotlinx.android.synthetic.main.activity_web.*
+
+class WebActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web)
+
+        this.wbvNavegador.settings.javaScriptEnabled = true
+        this.wbvNavegador.settings.pluginState = WebSettings.PluginState.ON
+        this.wbvNavegador.settings.allowFileAccess = true
+
+    }
+}
