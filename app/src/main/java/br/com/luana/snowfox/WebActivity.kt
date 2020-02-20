57e3cea1f6bb2308626f0e638878ec8e6bd47cb7
luana-vargas <vargas.luana@outlook.com.br>
2020-02-19 21:11:38 -0300

    Melhorias na webview

--- app/src/main/java/br/com/luana/snowfox/WebActivity.kt
+++ app/src/main/java/br/com/luana/snowfox/WebActivity.kt
@@ -1,9 +1,13 @@
 package br.com.luana.snowfox
 
 import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.webkit.WebSettings
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -37,17 +41,26 @@ class WebActivity : AppCompatActivity() {
         }
 
         edtUrl.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+            if (actionId == EditorInfo.IME_ACTION_GO ||
+                    actionId == EditorInfo.IME_ACTION_DONE ||
+                    event.action == KeyEvent.ACTION_DOWN &&
+                    event.keyCode == KeyEvent.KEYCODE_ENTER){
                 pesquisar()
+                v.hideKeyBoard()
                 true
             }
             false
         }
 
+        edtUrl.setSelectAllOnFocus(true)
+
     }
 
     fun pesquisar(){
-        val url = edtUrl.text.toString().trim()
+        var url = edtUrl.text.toString().trim()
+        if (!url.startsWith("http") || !url.startsWith("https")){
+            url = "https://" + url
+        }
         wbvNavegador.loadUrl(url)
     }
 
@@ -62,4 +75,9 @@ class WebActivity : AppCompatActivity() {
     fun home(){
         wbvNavegador.loadUrl(getString(R.string.url_padrao))
     }
+}
+
+fun View.hideKeyBoard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
 }
